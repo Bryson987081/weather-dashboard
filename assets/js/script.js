90e08e4cfcdc20f0bb5d1ec01c377bded5860115
Bryson <brysob7617@gmail.com>
2024-06-08 22:27:52 -0400

    added code to display data onto the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ searchForm.addEventListener('submit', function (event) {
 
     const q = qInput.value.trim();
 
-    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + q + '&appid=3c5908744bd76026f872444cdb7a76b6&units=imperial')
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + q + '&cnt=6&appid=3c5908744bd76026f872444cdb7a76b6&units=imperial')
         .then(function (response) {
             return response.json();
         })
@@ -15,16 +15,20 @@ searchForm.addEventListener('submit', function (event) {
             console.log('Search');
             console.log(data);
 
-            for (let result of data.list) {
+            for (let list of data.list) {
                 const columnEl = document.createElement('div');
                 const cardEl = document.createElement('div');
+                const cardHeaderEl = document.createElement('h2');
 
                 columnEl.className = 'col-12';
                 cardEl.className = 'card p-3 m-3';
-                cardEl.textContent = result.list;
-
+                cardHeaderEl.textContent = qInput;
+                cardEl.textContent = `${list.dt_txt}Temp: ${list.main.temp} Wind: ${list.wind.speed} Humidity: ${list.main.humidity}`; 
+                
+                columnEl.appendChild(cardHeaderEl);
                 columnEl.appendChild(cardEl);
                 resultList.appendChild(columnEl);
+                
             }
         })
         .catch(function (err) {
