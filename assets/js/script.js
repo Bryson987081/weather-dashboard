b6320be8a27b24790b61ce4eee85f3108e4bbba6
Bryson <brysob7617@gmail.com>
2024-06-10 18:08:40 -0400

    fixed code to only display dailly forecast instead of 3 hour increments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ const resultList = document.querySelector('#results');
 const recentSearch = document.querySelector('#recent-search');
 
 
-searchForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+
+const createCard = function () {
 
     const q = qInput.value.trim();
 
@@ -16,15 +16,18 @@ searchForm.addEventListener('submit', function (event) {
         .then(function (data) {
             console.log('Search');
             console.log(data);
+            const dataJSON = JSON.stringify(data)
+            localStorage.setItem('recent', dataJSON);
 
-            for (let list of data.list) {
+           // for (let list of data.list) {
+           for (let i = 0; i < data.list.length; i=i+8) {
+                const list = data.list[i];
                 const columnEl = document.createElement('div');
                 const cardDateEl = document.createElement('p');
                 const cardImgEl = document.createElement('a');
                 const cardTempEl = document.createElement('p');
                 const cardWindEl = document.createElement('p');
                 const cardHumidityEl = document.createElement('p');
-
 
                 columnEl.className = 'col card m-3 p-4';
                 cardDateEl.textContent = `${list.dt_txt}`;
@@ -41,7 +44,7 @@ searchForm.addEventListener('submit', function (event) {
                 resultList.appendChild(columnEl);
 
             }
-            
+
             const recentBtnDiv = document.createElement('div');
             const recentBtn = document.createElement('button')
             recentBtn.className = 'btn btn-primary col-2 m-3 p-2';
@@ -55,17 +58,28 @@ searchForm.addEventListener('submit', function (event) {
             console.log(err);
         });
 
-        searchForm.addEventListener('submit', function(event) {
-            event.preventDefault();
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-            const cardColumn = document.getElementById('results');
-            const cards = cardColumn.querySelectorAll('.card');
-            cards.forEach(card => {
-                card.remove();
-            })
+        const cardColumn = document.getElementById('results');
+        const cards = cardColumn.querySelectorAll('.card');
+        cards.forEach(card => {
+            card.remove();
         })
+    })
 
 
-        });
-        
-       
+};
+
+recentSearch.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const showRecent = localStorage.getItem("recent");
+    createCard(showRecent);
+
+
+})
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    createCard();
+
+});
