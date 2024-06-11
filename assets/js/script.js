63d765f01ef10442a97700a397fd6cdd18ef47c4
Bryson <brysob7617@gmail.com>
2024-06-10 21:40:36 -0400

    added code to display icon image

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,22 +19,24 @@ const createCard = function () {
             const dataJSON = JSON.stringify(data)
             localStorage.setItem('recent', dataJSON);
 
-           // for (let list of data.list) {
-           for (let i = 0; i < data.list.length; i=i+8) {
+            // for (let list of data.list) {
+            for (let i = 0; i < data.list.length; i = i + 8) {
                 const list = data.list[i];
                 const columnEl = document.createElement('div');
-                const cardDateEl = document.createElement('p');
-                const cardImgEl = document.createElement('a');
+                const cardDateEl = document.createElement('h3');
+                const cardImgEl = document.createElement('img');
                 const cardTempEl = document.createElement('p');
                 const cardWindEl = document.createElement('p');
                 const cardHumidityEl = document.createElement('p');
+                const icon = `${list.weather[0].icon}`;
 
                 columnEl.className = 'col card m-3 p-4';
-                cardDateEl.textContent = `${list.dt_txt}`;
-                cardImgEl.textContent = `https://openweathermap.org/img/wn/'${list.weather.icon}'@2x.png`;
-                cardTempEl.textContent = `Temp: ${list.main.temp} `;
-                cardWindEl.textContent = `Wind: ${list.wind.speed}`;
-                cardHumidityEl.textContent = `Humidity: ${list.main.humidity}`;
+                cardImgEl.className = "icon col-3";
+                cardDateEl.textContent = dayjs(list.dt_txt).format('MM/DD/YYYY');
+                cardImgEl.src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
+                cardTempEl.textContent = `Temp: ${list.main.temp} F°`;
+                cardWindEl.textContent = `Wind: ${list.wind.speed} MPH`;
+                cardHumidityEl.textContent = `Humidity: ${list.main.humidity} %`;
 
                 columnEl.appendChild(cardDateEl);
                 columnEl.appendChild(cardImgEl);
